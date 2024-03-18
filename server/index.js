e15d940eb3a1c0f45cfd407bac6a18ce4048e145
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-03-18 15:46:40 -0400

    Change PORT due to server issues

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = 8070;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
